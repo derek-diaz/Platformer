e2c85e46a2aaf26e4a753002e376e40492c96f91
Derek Diaz Correa <gigabit77@gmail.com>
2020-03-19 23:20:37 -0500

    Added Player and Tile system

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,13 +2,38 @@ extends KinematicBody2D
 
 var motion = Vector2(0,0)
 
-const SPEED = 500
+const SPEED = 1500
+const GRAVITY = 300
+const UP = Vector2(0,-1)
+const JUMP_SPEED = 5000
+
+signal animate
 
 func _physics_process(delta):
+	apply_gravity()
+	jump()
+	move()
+	animate()
+	move_and_slide(motion, UP)
+
+
+func apply_gravity():
+	if is_on_floor():
+		motion.y = 0
+	else:
+		motion.y += GRAVITY
+		
+func jump():
+	if Input.is_action_pressed("jump") and is_on_floor():
+		motion.y -= JUMP_SPEED
+
+func move():
 	if Input.is_action_pressed("Left") and not Input.is_action_pressed("Right"):
 		motion.x = -SPEED
 	elif Input.is_action_pressed("Right") and not Input.is_action_pressed("Left"):
 		motion.x = SPEED
 	else:
 		motion.x = 0
-	move_and_slide(motion)
+
+func animate():
+	emit_signal("animate", motion)
