b6278a526c12ce565df3d69ecdd64962de206f81
Derek Diaz Correa <gigabit77@gmail.com>
2020-03-20 23:16:45 -0500

    Game Over Screen added, Spikes added

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,6 +6,9 @@ const SPEED = 1500
 const GRAVITY = 300
 const UP = Vector2(0,-1)
 const JUMP_SPEED = 5000
+const WORLD_LIMIT = 4000
+
+var lives = 3
 
 signal animate
 
@@ -18,8 +21,12 @@ func _physics_process(delta):
 
 
 func apply_gravity():
+	if position.y > WORLD_LIMIT:
+		end_game()
 	if is_on_floor():
 		motion.y = 0
+	elif is_on_ceiling():
+		motion.y = 1
 	else:
 		motion.y += GRAVITY
 		
@@ -37,3 +44,14 @@ func move():
 
 func animate():
 	emit_signal("animate", motion)
+
+func end_game():
+	get_tree().change_scene("res://Levels/GameOver.tscn") 
+	
+func hurt():
+	position.y -= 1
+	yield(get_tree(), "idle_frame")
+	motion.y -= JUMP_SPEED
+	lives -= 1
+	if lives < 0:
+		end_game()
